efb48962f94443d066c1c358938b82d67dae72ee
Christian Garcia <kriz10@outlook.com>
2021-06-22 00:12:18 -0600

    modulo registro de usuario

--- Veterinaria/urls.py
+++ Veterinaria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,registro
 
 urlpatterns = [
     path('',index, name="index"),
+    path('registro/',registro, name="registro"),
 ]
--- Veterinaria/views.py
+++ Veterinaria/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,get_object_or_404
+from .forms import CustomUserCreationForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.http import Http404
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
-def index (request):
+def index(request):
     return render(request, 'index.html')
+
+def registro(request):
+    data={
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        formulario=CustomUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user=authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
+            login(request,user)
+            messages.success(request,"Registro exitoso")
+            return redirect(to="index")
+        data["form"]=formulario
+
+    return render(request, 'registration/registro.html',data)
